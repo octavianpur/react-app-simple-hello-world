59f548b858f4037a0c423f42deb50eff55bef301
octavianpur <octavian.p@gmail.com>
2022-01-16 12:27:11 +0200

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ const Title = () => {
   return (
     <div className="title">
       <span className="caption">
-        Hello <strong>World</strong>
+        Hello <strong>World</strong>!!!
       </span>
     </div>
   )
